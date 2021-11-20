a70d80a8315028bf5051921cc11832b75d74e8a1
Sulayman Musharraf <92084247+Mushrah-2019@users.noreply.github.com>
2021-11-20 08:13:37 +0100

    Update Task5.py

--- Task5.py
+++ Task5.py
@@ -68,4 +68,4 @@ print(my_dict['k1'][0]['nest_key'][1])
 
 #Question15...my_dict = {'k1':[1,2,{'k2':['this is tricky',{'tough':[1,2,['hello']]}]}]}...Grab hello
 my_dict = {'k1':[1,2,{'k2':['this is tricky',{'tough':[1,2,['hello']]}]}]}
-print(my_dict['k1'][2]['k2'][1]['tough'][2])   
+print(my_dict['k1'][2]['k2'][1]['tough'][2][0])   
